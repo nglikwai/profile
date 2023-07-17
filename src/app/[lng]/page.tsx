79d870680d469541680c562ece5780f18eb8f994
LIK WAI NG <nglikwai@ymail.com>
2023-07-17 14:59:44 +0800

    fix:i18n

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -7,16 +7,12 @@ import { Main } from "@/components/Main";
 import { ProjectProfile } from "@/components/ProjectProfile";
 import { NextPage } from "next";
 
-type props = {
-  params: {
-    lng: string
-  }
-}
-const Home: NextPage<props> = ({ params: { lng } }) => {
+
+const Home = () => {
   return (
     <PageWrapper>
-      <Main lng={lng} />
-      <ProjectProfile lng={lng} />
+      <Main />
+      <ProjectProfile />
     </PageWrapper>
 
   )
