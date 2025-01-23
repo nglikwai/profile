e9e6057b5365e714da4619aa84c208752bd71e97
LIK WAI NG <nglikwai@ymail.com>
2025-01-23 02:31:07 +0000

    feat: add preflight

--- src/app/api/func/route.ts
+++ src/app/api/func/route.ts
@@ -101,3 +101,13 @@ export async function POST(request: Request) {
     },
   );
 }
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    },
+  });
+}
